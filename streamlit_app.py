d96870e87fdf36d6f43f8ac4be848ff603d66e07
adlinadhivat1-lab <adlinadhivat1@gmail.com>
2026-05-22 14:20:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,19 +4,6 @@ st.title("🎈 Adlina's page")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-st.markdown("*Streamlit* is **really** ***cool***.")
-st.markdown('''
-    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
-    :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
-st.markdown("Here's a bouquet &mdash;\
-            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
-
-multi = '''If you end a line with two spaces,
-a soft return is used for the next line.
-
-Two (or more) newline characters in a row will result in a hard return.
-'''
-st.markdown(multi)
 st.page_link("your_app.py", label="Home", icon="🏠")
 st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
 st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣", disabled=True)
