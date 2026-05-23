e8aa5021ea46d5412c7a35409c1f9d44ee7b6e15
adlinadhivat1-lab <adlinadhivat1@gmail.com>
2026-05-23 08:33:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Adlina's page")
+st.title("🎈 Areaseva")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
