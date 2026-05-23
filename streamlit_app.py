42ef0aaa1aadafc947668e6202049bd4ff210aef
adlinadhivat1-lab <adlinadhivat1@gmail.com>
2026-05-23 10:40:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,14 @@ else:
     st.write("Goodbye")
 
 st.button("Reset", type="primary")
+
+from numpy.random import default_rng as rng
+
+df = rng(0).standard_normal((10, 1))
+col1, col2 = st.columns([3, 1])
+
+col1.subheader("A wide column with a chart")
+col1.line_chart(df)
+
+col2.subheader("A narrow column with the data")
+col2.write(df)
