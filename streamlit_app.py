87cfb33e169ed61544dff91f3aad2b1dba464dc5
adlinadhivat1-lab <adlinadhivat1@gmail.com>
2026-05-23 09:58:14 +0700

    Refactor streamlit_app.py for clarity and structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,3 @@
-# app.py — baris 13
-st.set_page_config(
-  page_title="Finance Dashboard",
-  layout="wide"
-)
+import streamlit as st
 
-# Hirarki teks
 st.title("📊 Dashboard")
-st.header("Laporan Bulanan")
-st.subheader("📈 Monthly Expenses")
-st.caption("Made with ❤️ using Streamlit")
